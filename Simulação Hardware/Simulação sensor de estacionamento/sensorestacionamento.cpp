9d0a119cbb2f59d7f064ff6965a812d4df73f0a1
Miguel Teixeira <miguel.tbmagalhaes@gmail.com>
2021-11-08 09:24:27 -0300

    Integraçao com BD
    Simulaçoes terminadas utilizando mysql c++ connector.

--- Simulação Hardware/Simulação sensor de estacionamento/sensorestacionamento.cpp
+++ Simulação Hardware/Simulação sensor de estacionamento/sensorestacionamento.cpp
@@ -1,360 +1,221 @@
+#include <mysql.h>
 #include <iostream>
 #include <stdlib.h>
 #include <time.h>
 #include <iomanip>
 #include <locale.h>
+#include <string>
 
-//matriz que contem as vagas
-int andar1[4][15];
-int andar2[4][15];
-int andar3[4][15];
-int andar4[4][15];
-int andar5[4][15];
+//declarando variaveis para acesso do bd
+char* usr = (char*)"root";
+char* pass = (char*)"******";
+char* db = (char*)"DavidCenter";
+
+int qstado;
+
+MYSQL mysql, * conn;
+MYSQL_RES result;
+MYSQL_ROW linha;
+//estado do query.
+int qstate;
+
+//conecta com banco de dados e imprime "VAGAS"
+void imprime_bd()
+{
+    MYSQL_RES* res;
+    conn = mysql_init(0);
+
+    conn = mysql_real_connect(conn, "localhost", usr, pass, db, 3306, NULL, 0);
+
+    if (conn) {
+        puts("Conexao com o Banco de Dados bem sucedida!");
+
+        std::string query = "SELECT * FROM VAGAS";
+        const char* q = query.c_str();
+        qstate = mysql_query(conn, q);
+        if (!qstate)
+        {
+            res = mysql_store_result(conn);
+            while (linha = mysql_fetch_row(res))
+            {
+                printf("ID: %s, Status: %s\n", linha[0], linha[1]);
+            }
+        }
+        else
+        {
+            std::cout << "Query falhou: " << mysql_error(conn) << std::endl;
+        }
+    }
+    else {
+        puts("Falha ao conectar com o Banco de dados!");
+    }
+
+}
+
+int insere_vaga(std::string id_vaga, int status_vaga)
+{
+    mysql_init(&mysql);
+    std::string instrucao = "UPDATE Vagas SET STATUS_VAGA = " + std::to_string(status_vaga) + " WHERE CODIGO_VAGA = '" + id_vaga + "';";
+    conn = mysql_real_connect(&mysql, "localhost", usr, pass, db, 0, NULL, 0);
+
+    if (conn == NULL)
+    {
+        std::cout << mysql_error(&mysql) << std::endl;
+    }
+   
+    else
+    {
+        (mysql_query(&mysql, instrucao.c_str()));
+        if (qstado != 0) 
+        {
+            std::cout << mysql_error(conn) << std::endl;
+            return 1;
+        }
+    }
+        mysql_close(&mysql);
+    }
 
 //funcao que valida int
 template<typename t>
-void valida_input(t&input, const std::string msg)
+void valida_input(t& input, const std::string msg)
 {
     //impede casos de cin.fail (so por precaucao):
+    std::cout << msg;
+    std::cin >> input;
+    while (std::cin.fail())
+    {
+        std::cout << "\n>> Erro de entrada, tente novamente.\n";
+        std::cin.clear();
+        std::cin.ignore(9999, '\n');
         std::cout << msg;
         std::cin >> input;
-        while (std::cin.fail())
-        {
-            std::cout<<"\n>> Erro de entrada, tente novamente.\n";
-            std::cin.clear();
-            std::cin.ignore(9999, '\n');
-            std::cout << msg;
-            std::cin >> input;
-        }           
+    }
 }
 
 //funcao do menu de opcoes (impressao e entrada)
 void menu_de_opcoes(int& opcao_menu)
 {
-    std::cout<<"\nMenu de opcoes\n\n";
-    std::cout<<"1 - Inserir vagas aleatorias\n";
-    std::cout<<"2 - Inserir '0' em todas as vagas\n";
-    std::cout<<"3 - Inserir '1' em todas as vagas\n";
-    std::cout<<"4 - Alterar vagas existentes\n";
-    std::cout<<"5 - Imprimir mapa de vagas\n";
-    std::cout<<"0 - Sair do programa\n";
+    std::cout << "\nMenu de opcoes\n\n";
+    std::cout << "1 - Inserir vagas aleatorias\n";
+    std::cout << "2 - Inserir '0' em todas as vagas\n";
+    std::cout << "3 - Inserir '1' em todas as vagas\n";
+    std::cout << "4 - Alterar vagas existentes\n";
+    std::cout << "5 - Imprimir mapa de vagas\n";
+    std::cout << "0 - Sair do programa\n";
     //validacao de entrada do menu:
-    do{
+    do {
         valida_input(opcao_menu, "\nDigite a opcao: ");
-        if (opcao_menu < 0 || opcao_menu > 5) std::cout<<"\n>>Insira apenas valores existentes no menu!\n";
+        if (opcao_menu < 0 || opcao_menu > 5) std::cout << "\n>>Insira apenas valores existentes no menu!\n";
     } while (opcao_menu < 0 || opcao_menu > 5);
-    
 
 }
 
-
-void insere_aleatorio()
-{ 
-    int x, i, j;
-    srand((unsigned)time(0));       //seed do numero gerado aleatoreamente 
-    
-    for (x=0; x<5; x++)
-    {
-        switch (x+1)
-        {
-            case 1:
-                for (i=0; i<4; i++)
-                {
-                    for (j=0; j<15; j++)
-                    {
-                        andar1[i][j] = rand()%2;
-                    }
-                }
-                break;
-            case 2:
-                for (i=0; i<4; i++)
-                {
-                    for (j=0; j<15; j++)
-                    {
-                        andar2[i][j] = rand()%2;
-                    }
-                }
-                break;
-            case 3:
-                for (i=0; i<4; i++)
-                {
-                    for (j=0; j<15; j++)
-                    {
-                        andar3[i][j] = rand()%2;
-                    }
-                }
-                break;
-            case 4:
-                for (i=0; i<4; i++)
-                {
-                    for (j=0; j<15; j++)
-                    {
-                        andar4[i][j] = rand()%2;
-                    }
-                }
-                break;
-            case 5:
-                for (i=0; i<4; i++)
-                {
-                    for (j=0; j<15; j++)
-                    {
-                        andar5[i][j] = rand()%2;
-                    }
-                }
-                break;
-        }
-        
-    }
-    std::cout<<"\n>>Vagas alteradas com sucesso!\n";
-}
-
-void insere_zero_ou_um(int n)
+void seleciona_todas_vagas(int opcao)
 {
-    int x, i, j;
+    int x, i, j, andar, vaga=0;
+    char letra;
+    std::string idvaga;
 
-    for (x=0; x<5; x++)
-    {
-        switch (x+1)
-        {
-            case 1:
-                for (i=0; i<4; i++)
-                {
-                    for (j=0; j<15; j++)
-                    {
-                        andar1[i][j] = n;
-                    }
-                }
-                break;
-            case 2:
-                for (i=0; i<4; i++)
-                {
-                    for (j=0; j<15; j++)
-                    {
-                        andar2[i][j] = n;
-                    }
-                }
-                break;
-            case 3:
-                for (i=0; i<4; i++)
-                {
-                    for (j=0; j<15; j++)
-                    {
-                        andar3[i][j] = n;
-                    }
-                }
-                break;
-            case 4:
-                for (i=0; i<4; i++)
-                {
-                    for (j=0; j<15; j++)
-                    {
-                        andar4[i][j] = n;
-                    }
-                }
-                break;
-            case 5:
-                for (i=0; i<4; i++)
-                {
-                    for (j=0; j<15; j++)
-                    {
-                        andar5[i][j] = n;
-                    }
-                }
-                break;
-        }
-        
-    }
-    std::cout<<"\n>>Vagas alteradas com sucesso!\n";
-}
-
-void imprime()
-{
-    char a;
-    int x,i,j;
     srand((unsigned)time(0));       //seed do numero gerado aleatoreamente 
-    
-    for (x=0; x<5; x++)
-    {
-        switch (x+1)
-        {
-            case 1:
-                std::cout << "Andar 1: \n";
-                for(i = 0; i < 4; i++)
-                {
-                    if (i == 0) a = 65;
-                    if (i == 1) a = 66;
-                    if (i == 2) a = 67;
-                    if (i == 3) a = 68;
-                    std::cout << "\n" << a << ": \n";
-                    for (j = 0; j < 15; j++)
-                    {
-                        std::cout << std::setw(5) << andar1[i][j];
-                    }
-                }
-                break;
-            case 2:
-                std::cout << "\n\nAndar 2: \n";
-                for(i = 0; i < 4; i++)
-                {
-                    if (i == 0) a = 65;
-                    if (i == 1) a = 66;
-                    if (i == 2) a = 67;
-                    if (i == 3) a = 68;
-                    std::cout << "\n" << a << ": \n";
-                    for (j = 0; j < 15; j++)
-                    {
-                        std::cout << std::setw(5) << andar2[i][j];
-                    }
-                }
-                break;
-            case 3:
-                std::cout << "\n\nAndar 3: \n";
-                for(i = 0; i < 4; i++)
-                {
-                    if (i == 0) a = 65;
-                    if (i == 1) a = 66;
-                    if (i == 2) a = 67;
-                    if (i == 3) a = 68;
-                    std::cout << "\n" << a << ": \n";
-                    for (j = 0; j < 15; j++)
-                    {
-                        std::cout << std::setw(5) << andar3[i][j];
-                    }
-                }
-                break;
-            case 4:
-                std::cout << "\n\nAndar 4: \n";
-                for(i = 0; i < 4; i++)
-                {
-                    if (i == 0) a = 65;
-                    if (i == 1) a = 66;
-                    if (i == 2) a = 67;
-                    if (i == 3) a = 68;
-                    std::cout << "\n" << a << ": \n";
-                    for (j = 0; j < 15; j++)
-                    {
-                        std::cout << std::setw(5) << andar4[i][j];
-                    }
-                }
-                break;
-            case 5:
-                std::cout << "\n\nAndar 5: \n";
-                for(i = 0; i < 4; i++)
-                {
-                    if (i == 0) a = 65;
-                    if (i == 1) a = 66;
-                    if (i == 2) a = 67;
-                    if (i == 3) a = 68;
-                    std::cout << "\n" << a << ": \n";
-                    for (j = 0; j < 15; j++)
-                    {
-                        std::cout << std::setw(5) << andar5[i][j];
-                    }
-                }
-                break;
-        }
-        
-    }
-    
-}    
 
+    for (x = 0; x < 5; x++)
+    {
+        andar = (x + 1);
+        for (i=0; i<4; i++)
+        {
+            switch (i)
+            {
+                case 0:
+                    letra = 'A';
+                    break;               
+                case 1:
+                    letra = 'B';
+                    break;                
+                case 2:
+                    letra = 'C';
+                    break;
+                case 3:
+                    letra = 'D';
+                    break;
+            }
+            
+            for (j = 0; j < 15; j++) 
+            {
+                if (j < 9) idvaga = std::to_string(andar) + letra + '0' + std::to_string(j + 1);
+                else idvaga = std::to_string(andar) + letra + std::to_string(j + 1);
+                if (opcao==1) insere_vaga(idvaga, (rand() % 2));
+                if (opcao==2)insere_vaga(idvaga, 0);
+                if (opcao == 3)insere_vaga(idvaga, 1);
+                //std::cout << std::endl << idvaga;
+            }
+        }
+
+    }
+    std::cout << "\n>>Vagas alteradas com sucesso!\n";
+}
 
 void alterar()
 {
-    int n_andar, vaga, blocon;
+    int n_andar, vaga, valor;
     char bloco;
+    std::string id_vaga;
     do
     {
         valida_input(n_andar, "Alterar vaga em qual andar? ");
-        if (n_andar < 1 || n_andar > 5) std::cout<<"\n>>Insira apenas valores entre 1 e 5!\n";
+        if (n_andar < 1 || n_andar > 5) std::cout << "\n>>Insira apenas valores entre 1 e 5!\n";
     } while (n_andar < 1 || n_andar > 5);
     do
     {
         valida_input(bloco, "Alterar vaga em qual bloco? ");
         bloco = toupper(bloco);
-        if (bloco<'A' || bloco>'D') std::cout<<"\n>>Insira apenas letras de 'A' a 'D'!\n";
-        switch (bloco)
-        {
-            case 65:
-                blocon = 0;
-                break;
-            case 66:
-                blocon = 1;
-                break;
-            case 67:
-                blocon = 2;
-                break;
-            case 68:
-                blocon = 3;
-                break;
-        }
-
-    } while (bloco<'A' || bloco>'D');
+        if (bloco < 'A' || bloco>'D') std::cout << "\n>>Insira apenas letras de 'A' a 'D'!\n";     
+    } while (bloco < 'A' || bloco>'D');
+    do
     {
         valida_input(vaga, "Alterar qual vaga (1-15)? ");
-        if (vaga<1 || vaga>15) std::cout<<"\n>>Insira apenas letras de 'A' a 'D'!\n";
-    } while (vaga<1 || vaga>15);
-    vaga--;
-    
-    switch (n_andar)
+        if (vaga < 1 || vaga>15) std::cout << "\n>>Insira apenas letras de 'A' a 'D'!\n";
+    } while (vaga < 1 || vaga>15);
+    do
     {
-        
-        case 1:
-            if (andar1[blocon][vaga]==0) andar1[blocon][vaga]=1;
-            else andar1[blocon][vaga]=0;
-            break;  
-        case 2:
-            if (andar2[blocon][vaga]==0) andar2[blocon][vaga]=1;
-            else andar2[blocon][vaga]=0;
-            break;
-        case 3:
-            if (andar3[blocon][vaga]==0) andar3[blocon][vaga]=1;
-            else andar3[blocon][vaga]=0;
-            break;
-        case 4:
-            if (andar4[blocon][vaga]==0) andar4[blocon][vaga]=1;
-            else andar4[blocon][vaga]=0;
-            break;
-        case 5:
-            if (andar5[blocon][vaga]==0) andar5[blocon][vaga]=1; 
-            else andar5[blocon][vaga]=0;
-            break;
-    }
+        valida_input(valor, "Inserir qual valor na vaga? ");
+        if (valor < 0 || valor > 1) std::cout << "\n>>Insira apenas valores entre 1 e 5!\n";
+    } while (valor < 0 || valor > 1);
 
-    std::cout<<"\n>>Vaga alterada com sucesso!\n";
+    if (vaga<10) id_vaga = std::to_string(n_andar) + bloco + '0' + std::to_string(vaga);
+    else id_vaga = std::to_string(n_andar) + bloco + std::to_string(vaga);
+    insere_vaga(id_vaga, valor);
+    std::cout << "\n>>Vaga alterada com sucesso!\n";
 }
 
 int main()
 {
-    int opcao, n, n_procurar,x, conta=0;
-    
-    
-    std::cout<<"Opcao digitada: "<<opcao; 
+    int opcao, n, n_procurar, x, conta = 0;
+
     do
     {
         //imprime menu
         menu_de_opcoes(opcao);
-        
-        switch(opcao)
+
+        switch (opcao)
         {
-            case 1:                 
-                insere_aleatorio();
-                break; 
-            case 2:                 
-                insere_zero_ou_um(0);
-                break;
-            case 3: 
-                insere_zero_ou_um(1);
-                break;
-            case 4: 
-                alterar();
-                break;
-            case 5:
-                imprime();
-                break;
-            default: std::cout<<"Erro. Fechando programa "; break;
+        case 1:
+            seleciona_todas_vagas(opcao);
+            break;
+        case 2:
+            seleciona_todas_vagas(opcao);
+            break;
+        case 3:
+            seleciona_todas_vagas(opcao);
+            break;
+        case 4:
+            alterar();
+            break;
+        case 5:
+            imprime_bd();
+            break;
+        default: std::cout << "Erro. Fechando programa "; break;
         }
-        
-    } while(opcao!=0);
+
+    } while (opcao != 0);
     return 0;
 }
--- Simulação Hardware/totems/entrada_e_saida.cpp
+++ Simulação Hardware/totems/entrada_e_saida.cpp
@@ -1,114 +1,231 @@
+#include <mysql.h>
 #include <iostream>
 #include <stdlib.h>
+#include <string>
 #include <time.h>
 #include <iomanip>
 #include <locale.h>
 
-int cod = 0; // variavel que guarda codigo (pegar ultimo codigo do bd)
+//declarando variaveis para acesso do bd
+char* usr = (char*)"root";
+char* pass = (char*)"*******";
+char* db = (char*)"DavidCenter";
+
+int qstado;
+
+MYSQL mysql, * conn;
+MYSQL_RES result;
+MYSQL_ROW linha;
+//estado do query.
+int qstate;
+
+//conecta com o bd e imprime todas as informacoes de ticket, sem considerar pagamentos, depois fecha a conexao
+void imprime_bd()
+{
+    MYSQL_RES* res;
+    conn = mysql_init(0);
+
+    conn = mysql_real_connect(conn, "localhost", usr, pass, db, 3306, NULL, 0);
+
+    if (conn) {
+        puts("Conexao com o Banco de Dados bem sucedida!");
+
+        std::string query = "SELECT * FROM CLIENTES";
+        const char* q = query.c_str();
+        qstate = mysql_query(conn, q);
+        if (!qstate)
+        {
+            res = mysql_store_result(conn);
+            while (linha = mysql_fetch_row(res))
+            {
+                printf("CODIGO_TICKET: %s, HORA_ENTRADA: %s, HORA_SAIDA: %s, STATUS_SAIDA: %s\n", linha[0], linha[1], linha[2], linha[3]);
+            }
+        }
+        else
+        {
+            std::cout << "Query falhou: " << mysql_error(conn) << std::endl;
+        }
+    }
+    else {
+        puts("Falha ao conectar com o Banco de dados!");
+    }
+}
+
+//conecta com o bd e roda um comando, depois fecha a conexao
+int rodacomando(std::string comando)
+{
+    mysql_init(&mysql);
+    std::string instrucao = comando;
+    conn = mysql_real_connect(&mysql, "localhost", usr, pass, db, 0, NULL, 0);
+
+    if (conn == NULL)
+    {
+        std::cout << mysql_error(&mysql) << std::endl;
+    }
+
+    else
+    {
+        (mysql_query(&mysql, instrucao.c_str()));
+        if (qstado != 0)
+        {
+            std::cout << mysql_error(conn) << std::endl;
+            return 1;
+        }
+    }
+    mysql_close(&mysql);
+}
+
+//conecta com o bd e checa se um codigo existe, depois fecha a conexao
+int checa_cod(std::string cod_compara)
+{
+    MYSQL_RES* res;
+    conn = mysql_init(0);
+
+    conn = mysql_real_connect(conn, "localhost", usr, pass, db, 3306, NULL, 0);
+
+    if (conn) {
+        puts("Conexao com o Banco de Dados bem sucedida!");
+
+        std::string query = "SELECT * FROM CLIENTES";
+        const char* q = query.c_str();
+        qstate = mysql_query(conn, q);
+        if (!qstate)
+        {
+            res = mysql_store_result(conn);
+            while (linha = mysql_fetch_row(res))
+            {
+                if (linha[0] == cod_compara) return 0;
+            }
+        }
+        else
+        {
+                std::cout << "Query falhou: " << mysql_error(conn) << std::endl;
+        }
+        }
+        else {
+            puts("Falha ao conectar com o Banco de dados!");
+    }
+    return 1;
+}
 
 //funcao que valida int
 template<typename t>
-void valida_input(t&input, const std::string msg)
+void valida_input(t& input, const std::string msg)
 {
     //impede casos de cin.fail (so por precaucao):
+    std::cout << msg;
+    std::cin >> input;
+    while (std::cin.fail())
+    {
+        std::cout << "\n>> Erro de entrada, tente novamente.\n";
+        std::cin.clear();
+        std::cin.ignore(9999, '\n');
         std::cout << msg;
         std::cin >> input;
-        while (std::cin.fail())
-        {
-            std::cout<<"\n>> Erro de entrada, tente novamente.\n";
-            std::cin.clear();
-            std::cin.ignore(9999, '\n');
-            std::cout << msg;
-            std::cin >> input;
-        }           
+    }
 }
 
 //funcao do menu de opcoes (impressao e entrada)
 void menu_de_opcoes(int& opcao_menu)
 {
-    std::cout<<"\nMenu de opcoes\n\n";
-    std::cout<<"1 - Simular uma entrada\n";
-    std::cout<<"2 - Simular uma saida\n";
-    std::cout<<"3 - Apagar tudo (inclui pagamentos)\n";
-    std::cout<<"0 - Sair do programa\n";
+    std::cout << "\nMenu de opcoes\n\n";
+    std::cout << "1 - Simular uma entrada\n";
+    std::cout << "2 - Simular uma saida\n";
+    std::cout << "3 - Imprimir BD\n";
+    std::cout << "4 - Apagar tickets (deixara pagamentos orfaos no bd)\n";
+    std::cout << "5 - Reiniciar contador de codigo de ticket\n";
+    std::cout << "6 - Apagar pagamentos (caso necessario)\n";
+    std::cout << "0 - Sair do programa\n";
     //validacao de entrada do menu:
-    do{
+    do {
         valida_input(opcao_menu, "\nDigite a opcao: ");
-        if (opcao_menu < 0 || opcao_menu > 3) std::cout<<"\n>>Insira apenas valores existentes no menu!\n";
-    } while (opcao_menu < 0 || opcao_menu > 3);
-    
+        if (opcao_menu < 0 || opcao_menu > 6) std::cout << "\n>>Insira apenas valores existentes no menu!\n";
+    } while (opcao_menu < 0 || opcao_menu > 6);
+
 
 }
 
-
+//simula uma entrada
 void entrada()
-{
-    cod = cod+1;
-   // data e hora baseadas nas config. locais
-   time_t agora = time(0);
-   // converte para string
-   char* hora_entrada = ctime(&agora);
-   std::cout << "Codigo do ticket simulado: " << cod << std::endl;
-   std::cout << "Data e hora de entrada: " << hora_entrada << std::endl;
-}
-
-void saida()
 {
     // data e hora baseadas nas config. locais
     time_t agora = time(0);
     // converte para string
-    char* hora_saida = ctime(&agora);
+    std::string hora_entrada = ctime(&agora);
+    //insere_bd_entrada(hora_entrada, "NEGADA");
+    rodacomando("insert into clientes (hora_entrada, status_saida) values ('" + hora_entrada + "', 'NEGADA'); ");
+}
+
+//simula uma saida
+void saida()
+{
+    int checa;
+    // data e hora baseadas nas config. locais
+    time_t agora = time(0);
+    std::string hora_saida = ctime(&agora);
     int cod_saida;
     valida_input(cod_saida, "Codigo do ticket: ");
-        //pegar o codigo do bd
-        /* */
-        //checar se a saida foi liberada pelo sistema (no momento vo simular isso)
+    //checar se a saida foi liberada pelo sistema (no momento vou simular isso)
+    checa = checa_cod(std::to_string(cod_saida));
+    if (checa == 1) std::cout << "Codigo nao existe no Banco de Dados!";
+    else
+    {
         int resposta;
-        do{
+        do {
             valida_input(resposta, "A saida do ticket foi liberada?(1=s/2=n)");
-            if (resposta < 1 || resposta > 2) std::cout<<"\n>>Insira apenas 1 para 's' ou 2 para 'n'!\n";
+            if (resposta < 1 || resposta > 2) std::cout << "\n>>Insira apenas 1 para 's' ou 2 para 'n'!\n";
         } while (resposta < 1 || resposta > 2);
 
         switch (resposta)
         {
-            case 1: //'S' - saida liberada
-                //escrever  cod ticket e hora de entrada 
-                std::cout << "Data e hora de saida: " << hora_saida << std::endl;
-                //envia pro bd
-                break;
+        case 1: //'S' - saida liberada
+            //escrever  cod ticket e hora de entrada 
+            //insere_bd_saida(hora_saida, "LIBERADA", std::to_string(cod_saida));
+            rodacomando("UPDATE clientes set hora_saida = ' " + hora_saida + "', status_saida = 'LIBERADA' WHERE CODIGO_TICKET = '" + std::to_string(cod_saida) + "';");
+            //envia pro bd
+            break;
 
-            case 2: //'N' - saida negada
-                std::cout << "O pagamento da taxa de estacionamento nao foi realizado! " << std::endl;
+        case 2: //'N' - saida negada
+            std::cout << "O pagamento da taxa de estacionamento nao foi realizado! " << std::endl;
             break;
         }
-
-
+    }
 }
+
 
 int main()
 {
     int opcao;
-    
-    
-    std::cout<<"Opcao digitada: "<<opcao; 
     do
     {
         //imprime menu
         menu_de_opcoes(opcao);
-        
-        switch(opcao)
+
+        switch (opcao)
         {
-            case 1:
-                entrada();                 
-                break; 
-            case 2:
-                saida();                 
-                break;
-            case 3: 
-                break;
-            default: std::cout<<"Erro. Fechando programa "; break;
+        case 1:
+            entrada();
+            break;
+        case 2:
+            saida();
+            break;
+        case 3:
+            imprime_bd();
+            break;
+        case 4:
+            rodacomando("DELETE FROM clientes;");
+            /*DELETE FROM clientes;
+            ALTER TABLE clientes AUTO_INCREMENT = 1;*/
+            break;
+        case 5: 
+            rodacomando("ALTER TABLE clientes AUTO_INCREMENT = 1;");
+            break;
+        case 6:
+            rodacomando("DELETE FROM taxas; ");
+            break;
+        default: std::cout << "Fechando programa. "; break;
         }
-        
-    } while(opcao!=0);
+
+    } while (opcao != 0);
     return 0;
 }
