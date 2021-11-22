7af4c9eea9964d14d6da591961017212ec3e5702
bouscs <busanix@gmail.com>
2021-11-22 10:41:50 -0300

    change db table names

--- Banco de Dados/DavidCenter(MySQL).sql
+++ Banco de Dados/DavidCenter(MySQL).sql
@@ -1,21 +1,18 @@
 create database DavidCenter;
 use DavidCenter;
-create table CLIENTES
+create table TICKETS
 ( CODIGO_TICKET int auto_increment, primary key(CODIGO_TICKET),
-  HORA_ENTRADA char(30),
-  HORA_SAIDA char(30),
+  HORA_ENTRADA datetime,
+  HORA_SAIDA datetime,
   STATUS_SAIDA char(30) not null);
   
-create table TAXAS
+create table PAGAMENTOS
 ( CODIGO_PAGAMENTO int auto_increment, primary key(CODIGO_PAGAMENTO),
-  HORA_SOLICITACAO char(30),
+  HORA_SOLICITACAO datetime,
   PRECO_TAXA float,
   CODIGO_TICKET_PAG integer,
   foreign key (CODIGO_TICKET_PAG) references CLIENTES (CODIGO_TICKET) );
-  
 
 create table VAGAS
 ( CODIGO_VAGA char(4) not null, primary key(CODIGO_VAGA),
   STATUS_VAGA int);
-  
-
