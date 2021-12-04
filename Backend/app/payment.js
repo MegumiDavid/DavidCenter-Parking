8c31a8a8fd1aabb22aa70699a2202a489f9cb1a7
João Ricardo <91494443+JOHN-RlCHARD@users.noreply.github.com>
2021-12-04 19:44:12 -0300

    update

--- Backend/app/payment.js
+++ Backend/app/payment.js
@@ -8,7 +8,9 @@ export function getPrice(ticket) {
   diff = diff / 60000
   var taxa = 0
 
-  if (diff < 60) {
+  if (diff <= 15) {
+    taxa = 0;
+  } else if (diff < 60) {
     taxa = preco.preco1
   } else if (diff >= 60 && diff < 120) {
     taxa = preco.preco2
